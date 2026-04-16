d623d883a4a5dbadbc0fb9f53af58886177a66b9
maulidaaa <maulidazahrasy>
2026-04-16 13:55:06 +0700

    update logout

---
+++ logout.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+$_SESSION = [];
+
+session_destroy();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+echo "<script>
+    alert('Anda telah berhasil keluar.');
+    window.location.href = 'index.php'; 
+</script>";
+exit();
+?>
