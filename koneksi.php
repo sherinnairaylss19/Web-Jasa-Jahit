08068a98429cdf46257fd72d802b15f413565099
sherinnairaylss19 <nairayulisasherin@gmail.com>
2026-04-16 13:28:34 +0700

    update login

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,30 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "toko-jahit";
+session_start();
+// Sesuaikan dengan nama database kamu
+$koneksi = mysqli_connect("localhost", "root", "", "nama_database_kamu");
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+if (mysqli_connect_errno()) {
+    echo "Koneksi database gagal : " . mysqli_connect_error();
+}
 
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Mencari user berdasarkan email (karena di tabelmu adanya email)
+$query = mysqli_query($koneksi, "SELECT * FROM users WHERE email='$username' AND password='$password'");
+$cek = mysqli_num_rows($query);
+
+if($cek > 0) {
+    $data = mysqli_fetch_assoc($query);
+    
+    $_SESSION['nama'] = $data['nama'];
+    $_SESSION['email'] = $data['email'];
+    $_SESSION['role'] = $data['role'];
+    $_SESSION['status'] = "login";
+
+    // Arahkan ke dashboard sesuai role
+    header("location:dashboard.php");
+} else {
+    header("location:index.php?pesan=gagal");
 }
 ?>
